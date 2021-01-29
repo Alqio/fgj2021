81074a1794fe69e7d61626d5e9e1eba887ae56cc
Ilmari Tarpila <leo.tarpila@aalto.fi>
2021-01-29 22:18:41 +0200

    Sweet sweet control stuff

---
+++ fgj2021/Assets/LeftCube.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LeftCube : MonoBehaviour
+{
+
+    public float moveSpeed = 1.5f;
+
+
+    Vector2 move;
+
+    PlayerControls controls;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        controls = new PlayerControls();
+
+        // controls.GameplaySticks.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
+        // controls.GameplaySticks.Move.canceled += ctx => move = Vector2.zero;
+
+        controls.GameplayKeyboard.WASD.performed += ctx => move = ctx.ReadValue<Vector2>();
+        controls.GameplayKeyboard.WASD.canceled += ctx => move = Vector2.zero;
+    }
+
+    void Update()
+    {
+        Vector2 m = new Vector2(move.x, move.y) * moveSpeed * Time.deltaTime;
+        transform.Translate(m, Space.World);
+    }
+
+    void OnEnable()
+    {
+        controls.GameplaySticks.Enable();
+        controls.GameplayKeyboard.Enable();
+
+    }
+
+    void OnDisable()
+    {
+        controls.GameplaySticks.Disable();
+        controls.GameplayKeyboard.Disable();
+
+    }
+
+}
