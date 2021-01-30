3d5bbbdcf320f3cfb8d8546f1b01aa0921cced8b
Tomi Panula <tomi.panula@visma.fi>
2021-01-30 10:54:32 +0200

    initial wind

---
+++ fgj2021/Assets/WindEffect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindEffect : MonoBehaviour
+{
+    public float thrust;
+    private Rigidbody2D rb;
+    public GameObject highPressure, lowPressure;
+    public Collider2D windCollider;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (rb.IsTouching(windCollider))
+        {
+            rb.AddForce(
+                (highPressure.transform.position - lowPressure.transform.position) *
+                NormalizeDist(Vector2.Distance(lowPressure.transform.position, highPressure.transform.position), 15) *
+                thrust *
+                Time.deltaTime, ForceMode2D.Impulse
+            );
+        }
+    }
+
+    float NormalizeDist(float dist, float range)
+    {
+        float value = -1 / range * dist + 1;
+        if (value > 1) return 0;
+        return value;
+    }
+
+}
