d4df17543f18a3e895dc9a31a1d16fe4c8bb47ce
alqio <kyosti.alkio@gmail.com>
2021-01-30 21:52:07 +0200

    Adds rockpiles

---
+++ fgj2021/Assets/Scripts/RockCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockCollider : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.gameObject.tag);
+        
+        if (other.gameObject.tag == "Lifeboat")
+        {
+            Destroy(other.gameObject);
+            GameManagerScript.Instance.lifeBoatDeath(other.GetComponent<Variables>().personName);
+        }
+        else if (other.gameObject.tag == "Boat")
+        {
+            other.gameObject.GetComponent<ShipMovement>().DestroyBoat();
+        }
+    }
+}
