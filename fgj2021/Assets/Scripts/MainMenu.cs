de8b0256d665d496d81d1bee11ae2dd44209bdf3
Laaden <lassi.knuuttila@aalto.fi>
2021-01-30 17:14:31 +0200

    Tehty menut ja animoitu tausta

---
+++ fgj2021/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    void Update()
+    {
+
+    }
+
+    public void Start()
+    {
+        SceneManager.LoadScene("LassinMainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quitting");
+        Application.Quit();
+    }
+}
